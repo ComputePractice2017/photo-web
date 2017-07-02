3b70ab06fe2ab7066bff15ff79a386dccba1919b
Damir <damir400@mail.ru>
2017-07-02 14:59:42 +0300

    Добавил увеличение фото + внутри работающая кнопка с функцией удаления фото issue #6

--- src/components/Lenta.vue
+++ src/components/Lenta.vue
@@ -5,15 +5,16 @@
             <br>
             <div>
                 <b-btn id="add_photo" @click="$root.$emit('show::modal','modal1')">Добавить фотографию</b-btn>
-                <b-modal id="modal1" title="Заполните данные" @ok="submit" @shown="clearName" hide-footer>
+                <!--Модальное окно для вывода формы добавления фото-->
+                <b-modal id="modal1" title="Заполните данные" hide-footer>
                     <form>
                         <div class="form-group">
                             <label class="sr-only" for="inlineFormInput">Имя</label>
-                            <input type="text" v-model="newphoto.name" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Введите имя">
+                            <input type="text" v-model="newphoto.name" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Введите имя" required>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputFile">Выбирите фотографию</label>
-                            <input type="file" accept="image/*" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+                            <input type="file" accept="image/*" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp" required>
                         </div>
                         <div class="modal-footer">
                         <button v-on:click="addNewPhoto" class="btn btn-primary pull-right">Добавить</button>
@@ -22,14 +23,24 @@
                 </b-modal>
             </div>
             <br>
+            <!--Галерея-->
             <div class="row">
                 <div class="col-md-3" v-for="photo in photos" style="width: 300px !important;">
-                    <img :src=" photo.url " alt="" class="img-thumbnail photo">
+                    <img :src="photo.url" class="img-thumbnail photo"  @click="showphoto(photo)&$root.$emit('show::modal','modal2')">
                     <p>{{ photo.name }}</p>
                 </div>
             </div>
-            <br>
-            {{newphoto}}
+            <!--Модальное окно для вывода изображения-->
+            <b-modal id="modal2" title="" hide-footer hide-header>
+                <img class="bigPhoto" :src="sphoto.url" alt="">
+                <div class="comment">
+                    <div id="knopkiphoto">
+                        <button class="btn" @click="deletephoto(sphoto)&$root.$emit('hide::modal','modal2')">Удалить запись</button>
+                    </div>
+                    <p id="namephoto">{{sphoto.name}}</p>
+                    <p id="commentphoto">Место для комментов</p>
+                </div> 
+            </b-modal>
         </div>
     </div>
 </template>
@@ -67,11 +78,36 @@
             {
               'name': 'Alice A.',
               'url': 'https://im0-tub-ru.yandex.net/i?id=ed26a62c3c6815fb43ffbe8532ac2df2-l&n=13'
+            },
+            {
+              'name': 'Pasha M.',
+              'url': 'https://im0-tub-ru.yandex.net/i?id=c7cc03c8d3e4277681ceada8d2565663-l&n=13'
+            },
+            {
+              'name': 'Vitya S.',
+              'url': 'http://www.youwall.com/wallpapers/201307/purple-flowers-wallpaper.jpg'
+            },
+            {
+              'name': 'Dima S.',
+              'url': 'https://i.obozrevatel.com/8/1747448/gallery/919603.jpg'
+            },
+            {
+              'name': 'Sveta Ya.',
+              'url': 'http://imagen46.com/images/2014/02/02/Fondos-de-escritorio-paisajes-forest-grass-green-lake-paisaje-mountains-nature-1080x1920.jpg'
+            },
+            {
+              'name': 'Alice D.',
+              'url': 'https://im0-tub-ru.yandex.net/i?id=ef68548c946ca3a0b5f56a222eecfecd-l&n=13'
             }
           ],
           newphoto: {
             'name': '',
             'url': ''
+          },
+          sphoto: {
+            'name': '',
+            'url': '',
+            'index': ''
           }
         }
       },
@@ -84,12 +120,22 @@
           obj.name = this.newphoto.name
           obj.url = this.newphoto.url
           this.photos.push(obj)
+        },
+        showphoto: function (obj) {
+          this.sphoto.url = obj.url
+          this.sphoto.name = obj.name
+          this.sphoto.index = this.photos.indexOf(obj)
+        },
+        deletephoto: function (obj) {
+          if (obj.index > -1) {
+            this.photos.splice(obj.index, 1)
+          }
         }
       }
     }
 </script>
 
-<style scoped>
+<style>
     h1{
         text-align: center;
     }
@@ -100,5 +146,38 @@
     #add_photo{
         display: block !important;
         margin: 0 auto;
+    }
+    #modal2 > .modal-dialog.modal-md {
+        max-width: 1100px;
+        margin: 15% auto;
+    }
+    #modal2_modal_body {
+        max-width: 1100px;
+        margin: 0;
+        padding: 0;
+        display: inline-block;
+    }
+    #modal2 {
+        background-color: #000000b3;
+    }
+    .bigPhoto {
+        max-width: 799px;
+    }
+    .comment {
+        width: 299px;
+        float: right;
+        text-align: center;
+    }
+    #namephoto {
+        padding: 10px;
+        margin: 0;
+        border-bottom: 1px solid #eee;
+    }
+    #commentphoto {
+        padding: 10px;
+    }
+    #knopkiphoto {
+        padding: 10px;
+        border-bottom: 1px solid #eee;
     }
 </style>
