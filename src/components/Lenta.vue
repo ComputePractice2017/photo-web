06fe9eebf5e6a22e73c77167e3abd5183448db64
Damir <damir400@mail.ru>
2017-07-02 19:22:43 +0300

    Паша добавил сверху навбар  + я добавил функцию поиска issue #7

--- src/components/Lenta.vue
+++ src/components/Lenta.vue
@@ -1,7 +1,26 @@
 <template>
     <div class="photos">
+        <nav class="navbar navbar-toggleable-md navbar bg" style="border-bottom:1px solid rgba(0,0,0,.0975); background-color:#fff; ">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="container">
+                <div class="col-md-3">
+                    <a class="navbar-brand" href="#">InstaГрам</a>
+                </div>
+                <div class="col-md-6 search">
+                    <input class="form-control_costom" type="text" placeholder="Поиск" v-model="search">
+                </div>
+                <div class="col-md-3">
+                    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                        <a href="#"><img src="../assets/img/friendf.png" alt="Найти друзей" class="icons"></a>
+                        <a href="#"><img src="../assets/img/likes.png" alt="Мои лайки" class="icons"></a>
+                        <a href="#"><img src="../assets/img/myprofile.png" alt="Мой профиль" class="icons"></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
         <div class="container">
-            <h1>InstaГрам</h1>
             <br>
             <div>
                 <b-btn id="add_photo" @click="$root.$emit('show::modal','modal1')">Добавить фотографию</b-btn>
@@ -25,7 +44,7 @@
             <br>
             <!--Галерея-->
             <div class="row">
-                <div class="col-md-3" v-for="photo in photos" style="width: 300px !important;">
+                <div class="col-md-3" v-for="photo in photoList" style="width: 300px !important;">
                     <img :src="photo.url" class="img-thumbnail photo"  @click="showphoto(photo)&$root.$emit('show::modal','modal2')">
                     <p>{{ photo.name }}</p>
                 </div>
@@ -108,7 +127,20 @@
             'name': '',
             'url': '',
             'index': ''
+          },
+          search: ''
+        }
+      },
+      computed: {
+        photoList: function () {
+          var search = this.search
+          var filterfn = function (item) {
+            return item.name.includes(search)
           }
+          if (this.search !== '') {
+            return this.photos.filter(filterfn)
+          }
+          return this.photos
         }
       },
       methods: {
@@ -179,5 +211,31 @@
     #knopkiphoto {
         padding: 10px;
         border-bottom: 1px solid #eee;
+    }
+    .search {
+        text-align: center;
+    }
+
+    .form-control_costom {
+        padding: 0.5rem 0.75rem;
+        font-size: 1rem;
+        line-height: 1.25;
+        color: #464a4c;
+        background-color: #fff;
+        background-image: none;
+        -webkit-background-clip: padding-box;
+        background-clip: padding-box;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        border-radius: 0.25rem;
+    }
+
+    a {
+        color: #000;
+    }
+
+    .icons{
+        margin: 10px 10px ;
+        width:25px;
+        height:24px;
     }
 </style>
